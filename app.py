cc452a93f2707b1b5c0d270efb6107603f9d920b
jshields <johnnyfivefivefive@protonmail.com>
2016-09-25 10:30:04 -0400

    Formatted python code

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # Data lookup table
-    pia = { 'hx88wtn92ppwgh4 katey bday':'September 3', 'hx88wtn92ppwgh4 carol bday'
-           :'January 12', 'hx88wtn92ppwgh4 dan bday':'July 21', 'hx88wtn92ppwgh4 anniv'
-           :'August 17 (2012)' }
+    pia = {
+        'hx88wtn92ppwgh4 katey bday': 'September 3',
+        'hx88wtn92ppwgh4 carol bday': 'January 12',
+        'hx88wtn92ppwgh4 dan bday': 'July 21',
+        'hx88wtn92ppwgh4 anniv': 'August 17 (2012)'
+    }
     result = ''
     if request.method == 'POST':
         # Get the user's query from the form
