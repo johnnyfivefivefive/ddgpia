3ae63f715475e5278fc722c064d5dc0719b1e3ab
jshields <johnnyfivefivefive@protonmail.com>
2016-09-26 09:01:12 -0400

    Added youtube link, duplicated for case match

--- app.py
+++ app.py
@@ -13,7 +13,16 @@ def home():
         'Hx88Wtn92Ppwgh4 anniv': 'August 17 (2012)',
         'Hx88Wtn92Ppwgh4 lentil soup': 'lentils, celery, onions, carrots, broth, tomato paste, red wine, thyme, salt, pepper',
         'Hx88Wtn92Ppwgh4 cornerstone': '031207830',
-        'Hx88Wtn92Ppwgh4 shows': 'black mirror'
+        'Hx88Wtn92Ppwgh4 shows': 'black mirror',
+        'Hx88Wtn92Ppwgh4 spinach': 'Trigon commercial: https://www.youtube.com/watch?v=a189xAYBRv8',
+        'hx88wtn92ppwgh4 katey bday': 'September 3',
+        'hx88wtn92ppwgh4 carol bday': 'January 12',
+        'hx88wtn92ppwgh4 dan bday': 'July 21',
+        'hx88wtn92ppwgh4 anniv': 'August 17 (2012)',
+        'hx88wtn92ppwgh4 lentil soup': 'lentils, celery, onions, carrots, broth, tomato paste, red wine, thyme, salt, pepper',
+        'hx88Wtn92Ppwgh4 cornerstone': '031207830',
+        'hx88wtn92ppwgh4 shows': 'black mirror',
+        'hx88wtn92ppwgh4 spinach': 'Trigon commercial: https://www.youtube.com/watch?v=a189xAYBRv8',
     }
     result = ''
     if request.method == 'POST':
